4dc9f991121f2c2ef394fe8551c4854c234221ae
sangaryousmane <sangaryousmane@gmail.com>
2023-08-10 08:19:39 -0700

    Complete command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    args = argv[1:]
+    count = len(args)
+    i = 0
+
+    if count == 0:
+        print("0 argument")
+    elif count == 1:
+        print("1 argument")
+    else:
+        print("{} arguments".format(count))
+
+    while i in range(count):
+        print("{}: {}".format(i + 1, args[i]))
+        i += 1
