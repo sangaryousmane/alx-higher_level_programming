89a87270e96aa4c3275c681b29cb2b8c4d34245b
sangaryousmane <sangaryousmane@gmail.com>
2023-08-11 09:10:58 -0700

    Fix pycodestyle

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,9 +2,8 @@
 if __name__ == "__main__":
     from calculator_1 import sub, add, mul, div
 
-    a = 10 
+    a = 10
     b = 5
-    
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
