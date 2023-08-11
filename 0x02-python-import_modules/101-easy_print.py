c6cb4e4717238429619d06167d467ee3185a5d53
sangaryousmane <sangaryousmane@gmail.com>
2023-08-11 09:40:41 -0700

    Modify line 3

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from os import write
-    write(1, "#pythoniscool\n".encode("UTF-8"))
+from os import write
+write(1, "#pythoniscool\n".encode("UTF-8"))
