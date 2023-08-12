ec9ee7f81ba2f7bb3fef6eb8f9bf9780db975ca4
sangaryousmane <sangaryousmane@gmail.com>
2023-08-12 09:12:58 -0700

    Multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    a, b = len(sentence), sentence[0]
-
+    
+    if sentence == '':
+        a, b = len(sentence), None
+    else:
+        b = sentence[0]
     return (a, b)
