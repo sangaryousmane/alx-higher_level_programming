90af8f8d676f758f12a919ce60de28165e94b72f
sangaryousmane <sangaryousmane@gmail.com>
2023-08-12 08:28:14 -0700

    Reverse numbers in one line

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    return my_list[::-1]
+    reverse =  my_list[::-1]
+
+    for item in reverse:
+        print("{:d}".format(item))
