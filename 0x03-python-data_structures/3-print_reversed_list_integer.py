f32e534cea72688aa6c513973807b526e48ca363
sangaryousmane <sangaryousmane@gmail.com>
2023-08-12 08:30:40 -0700

    Fix pycode error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reverse =  my_list[::-1]
+    reverse = my_list[::-1]
 
     for item in reverse:
         print("{:d}".format(item))
