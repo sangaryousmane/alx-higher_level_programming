a071834deccf8966412269a274023f762ae04e28
Ousmane <88728076+sangaryousmane@users.noreply.github.com>
2023-08-13 00:23:36 +0800

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == '':
-        char_1 = None
+        b = None
     else:
-        char_1 = sentence[0]
-        a, b = len(sentence), char_1
-        return (a, b)
+        b = sentence[0]
+        return (len(sentence), b)
