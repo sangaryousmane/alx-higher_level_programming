b29f9c6ac0fee79ace04f6ed68ed38849000aa3a
sangaryousmane <sangaryousmane@gmail.com>
2023-08-14 05:24:29 -0700

    Fix code to handle integeres matrix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+
     if sentence == '':
         b = None
     else:
         b = sentence[0]
-        return (len(sentence), b)
+    return (len(sentence), b)
