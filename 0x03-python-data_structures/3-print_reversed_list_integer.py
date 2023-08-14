05bb2453d85e0cb7af8669dffbd0a55f3179de7c
sangaryousmane <sangaryousmane@gmail.com>
2023-08-14 04:20:19 -0700

    Fix pycode style and truthness of the original list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
 
     """ Print list in reverse """
-    reverse = my_list[::-1]
-    if reverse:
+    if my_list:
+        reverse = my_list[::-1]
         for item in reverse:
             print("{:d}".format(item))
