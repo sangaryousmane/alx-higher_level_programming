436dd371d10aaf040ff923ef2704beae8f729517
sangaryousmane <sangaryousmane@gmail.com>
2023-08-16 06:37:01 -0700

    Done with this

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    """ Delete a key with specific value in a dictionary """
-
     to_del = []
     for k in a_dictionary:
         if a_dictionary[k] == value:
-            to_del.add(k)
+            to_del.append(k)
     for k in to_del:
         del a_dictionary[k]
     return a_dictionary
