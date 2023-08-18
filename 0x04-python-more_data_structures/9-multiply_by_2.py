8d48456ccdef2eb8688992631963485cf49b4442
sangaryousmane <sangaryousmane@gmail.com>
2023-08-18 05:41:11 -0700

    deletes a key in a dictionary.

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = dict()
     for key, value in a_dictionary.keys():
         value *= 2
+        new_dict[key] = value
     return a_dictionary
