8a3cd9dd20840abdfc68849f6bf5a5ddd0888899
sangaryousmane <sangaryousmane@gmail.com>
2023-08-18 05:43:52 -0700

    a new dictionary with all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for key, value in a_dictionary.keys():
-        value *= 2
-        new_dict[key] = value
+        new_dict[key] = value * 2
     return a_dictionary
