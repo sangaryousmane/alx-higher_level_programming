dd6a588c3465827bd24f2c1afea992d0e96a42e5
sangaryousmane <sangaryousmane@gmail.com>
2023-08-19 04:41:59 -0700

    Fix code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    """
-    This function adds 2 tuples. zip() takes two tuples
-    as arguments and perform addition on them using the
-    map() higher order function. Finally, the tuple()
-    converts the operation to tuples and return it.
-    """
-
     if len(tuple_a) < 2:
-        tuple_a += (0,) * (2 - len(tuple_a))
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
     if len(tuple_b) < 2:
-        tuple_b += (0,) * (2 - len(tuple_b))
-    sum_tuple = tuple(map(sum, zip(tuple_a, tuple_b)))
-    return added_tuple
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+    sum_tuple = (tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])
+    return (sum_tuple)
