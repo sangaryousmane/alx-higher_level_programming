199b1a951100a93b5e00189c0c160609f15403bf
sangaryousmane <sangaryousmane@gmail.com>
2023-08-19 04:26:45 -0700

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1 as cal
-    from sys import argv
+    import sys
+    from calculator_1 import add, sub, mul, div
 
-    args = argv[1:]
-    a = int(args[1])
-    b = int(args[3])
-    sign = args[2]
-    sign_list = ["*", "-", "/", "+"]
-
-    if len(args) != 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    else:
-        if sign not in sign_list:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        else:
-            if sign == sign_list[0]:
-                print("{} * {} = {}".format(a, b, cal.mul(a, b)))
-            elif sign == sign_list[1]:
-                print("{} - {} = {}".format(a, b, cal.sub(a, b)))
-            elif sign == sign_list[2]:
-                print("{} / {} = {}".format(a, b, cal.div(a, b)))
-            else:
-                print("{} + {} = {}".format(a, b, cal.add(a, b)))
+        sys.exit(1)
 
+    a = int(sys.argv[1])
+    sign = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if sign == "+":
+        result = add(a, b)
+    elif sign == "-":
+        result = sub(a, b)
+    elif sign == "*":
+        result = mul(a, b)
+    elif sign == "/":
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    print("{} {} {} = {}".format(a, sign, b, result))
