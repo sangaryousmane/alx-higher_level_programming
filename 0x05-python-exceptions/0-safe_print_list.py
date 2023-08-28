a9a24ff3f05c6a4f0fc15464434112c4395b0d9c
sangaryousmane <sangaryousmane@gmail.com>
2023-08-28 16:54:03 -0700

    Error amended

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     result = 0
     for num in range(x):
         try:
-            print(f'{}'.format(my_list[num]), end="")
+            print('{}'.format(my_list[num]), end="")
             result += 1
         except IndexError:
             break
