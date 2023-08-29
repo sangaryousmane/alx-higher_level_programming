2b4e58ed2894d892b7ab5920086d6e8a9722d63b
sangaryousmane <sangaryousmane@gmail.com>
2023-08-28 21:38:58 -0700

    Implement the private instance field

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This class implements an empty Square that defines a square
+    """
+    pass
+
+    def __init__(self, size):
+        self.__size = size
+        pass
