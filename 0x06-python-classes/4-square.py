ac7c7e65408cd6c108d204e221c02a0a6f8c3ab7
sangaryousmane <sangaryousmane@gmail.com>
2023-08-29 19:34:04 -0700

    Fix pycodestyle

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -28,4 +28,8 @@ class Square:
     @size.setter
     def size(self, value):
         """ the mutator that set a size for us"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
