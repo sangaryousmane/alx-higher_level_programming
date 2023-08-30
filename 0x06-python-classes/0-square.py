e91140dfefbd5dad417bb0bda3aba6e99866c2f0
sangaryousmane <sangaryousmane@gmail.com>
2023-08-29 18:40:07 -0700

    Fix code

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 class Square:
-    """
-    This class implements an empty Square that defines a square
-    """
-
-    pass
+    """This class implements an empty Square that defines a square"""
