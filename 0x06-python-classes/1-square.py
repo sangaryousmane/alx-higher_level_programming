afc1395779f64c89ac716b83dfbf93ee950e3049
sangaryousmane <sangaryousmane@gmail.com>
2023-08-29 18:47:52 -0700

    Fix pycode

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 class Square:
-    """
-    This class implements an empty Square that defines a square
-    """
+    """This class implements an empty Square that defines a square"""
 
     def __init__(self, size):
-        """
+        """this is the class constructor
         Args:
          size: the input size of the square.
         """
         self.__size = size
-        pass
