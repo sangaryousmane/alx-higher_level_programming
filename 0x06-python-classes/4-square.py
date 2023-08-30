efc9c2bc35a4560161aacc385957a2da64d4430c
sangaryousmane <sangaryousmane@gmail.com>
2023-08-29 19:16:18 -0700

    Complete getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Emulates a square"""
+
+
+class Square:
+    """This class implements a  Square that defines a square"""
+
+    def __init__(self, size=0):
+        """this is the class constructor
+        Args:
+         size: the input size of the square.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Calculate the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """ a public setter metter to retrieve the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ the mutator that set a size for us"""
+        self.__size = size
