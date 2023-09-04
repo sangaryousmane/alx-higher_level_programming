92ef46a323570d0ce1c8c7c14297a079df63eaf8
sangaryousmane <sangaryousmane@gmail.com>
2023-09-04 04:19:39 -0700

    Add empty Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Mimics a Rectangle """
+
+
+class Rectangle:
+    """"
+    This class represents a rectangle shape.
+
+    Attributes:
+    None
+
+    Methods:
+    None
+    
+    """
+    pass
