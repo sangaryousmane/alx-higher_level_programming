a033e835ef711520ab1015a0f00c665e72c3775b
sangaryousmane <sangaryousmane@gmail.com>
2023-09-04 05:29:55 -0700

    Reformat and fix errors

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -36,7 +36,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ sets the width's value """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -50,7 +50,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ The heigth's mutator """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
