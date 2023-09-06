b09191009beff5cbed046510aeae30f166152d1b
sangaryousmane <sangaryousmane@gmail.com>
2023-09-05 19:50:01 -0700

    Fix requirement

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -4,4 +4,4 @@ def magic_string():
     if not hasattr(magic_string, 'count'):
         magic_string.count = 0
     magic_string.count += 1
-    return ", ".join(["BestSchool" for i in range(magic_string.count)])
+    return (", ".join(["BestSchool" for i in range(magic_string.count)]))
