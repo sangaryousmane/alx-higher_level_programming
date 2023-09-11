38f73be3c93017f6728fdcf918c6ba8fbb0e9a17
sangaryousmane <sangaryousmane@gmail.com>
2023-09-11 16:50:32 -0700

    Fix pycodestyle

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Returns the list of available attributes and methods of an object"""
+
+
 def lookup(obj):
     """
     Returns a list of the names of the attributes and methods of an object.
