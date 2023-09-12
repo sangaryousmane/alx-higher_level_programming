781d6c4d755f8481a5f0f455db13e8ebdf3085e6
sangaryousmane <sangaryousmane@gmail.com>
2023-09-11 18:44:52 -0700

    Handle int validations

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ A derive class to the geometry class"""
-from 7-base_geometry import BaseGeometry
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ Inherits from the base Geometry class"""
 
     def __init__(self, width, height):
-        self.integer_validator(width, height)
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
