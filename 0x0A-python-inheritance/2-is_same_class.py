f1c893669a4098463578ffdac351413103b7a21a
sangaryousmane <sangaryousmane@gmail.com>
2023-09-11 17:52:20 -0700

    isinstance and type is completetly different

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,7 +6,4 @@ otherwise False.
 
 def is_same_class(obj, a_class):
     """ Checks for an instance"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) is a_class
