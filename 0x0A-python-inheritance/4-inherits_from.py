e0ded73040e83239e5dcd12cd196866af9bbfb5c
sangaryousmane <sangaryousmane@gmail.com>
2023-09-11 18:18:06 -0700

    subclass relationship

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,7 @@ def inherits_from(obj, a_class):
     inherited (directly or indirectly) from the specified class
     ;otherwise False
     """
-    return issubclass(obj, a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
