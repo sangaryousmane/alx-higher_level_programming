e3e52fe2e2c78b8146ad4bb6c2405c5df3c5efc0
sangaryousmane <sangaryousmane@gmail.com>
2023-09-11 18:05:00 -0700

    Add upper docstring

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" checks for subclass"""
+
+
 def inherits_from(obj, a_class):
     """returns True if the object is an instance of a class that
     inherited (directly or indirectly) from the specified class
