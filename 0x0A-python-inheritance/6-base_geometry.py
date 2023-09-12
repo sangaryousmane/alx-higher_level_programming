6b481cdaa235e6087a9b2fd13cbdb143e6a0bcd3
sangaryousmane <sangaryousmane@gmail.com>
2023-09-11 18:21:15 -0700

    throws an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Mimics a base class geometry"""
+
+
+class BaseGeometry:
+    """ Serve as super class for future child classes"""
+    
+
+    def area(self):
+        """ Raise an exception"""
+        raise Exception("area() is not implemented")
