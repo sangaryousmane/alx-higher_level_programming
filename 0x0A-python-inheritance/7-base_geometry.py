080fbb014cbf1027cc97e03d517eef264eb5b385
sangaryousmane <sangaryousmane@gmail.com>
2023-09-11 18:27:48 -0700

    Added value Error handlerD

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Mimics a base class geometry"""
+
+
+class BaseGeometry:
+    """ Serve as super class for future child classes"""
+
+    def area(self):
+        """ Raise an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validates an integer"""
+        if type(value) != int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
