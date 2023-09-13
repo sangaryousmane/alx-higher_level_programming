bea89871faa86bea856e351b75844661f9378c6f
sangaryousmane <sangaryousmane@gmail.com>
2023-09-12 17:37:46 -0700

    Amend code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """ Reads and prints the file content to stdout"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        data = f.read()
+        print(data, end='')
