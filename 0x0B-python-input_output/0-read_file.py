ac3b744292f54d6375bc2f2790f2ef3cd201526e
sangaryousmane <sangaryousmane@gmail.com>
2023-09-12 17:35:15 -0700

    Read file content

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" reads a text file (UTF8) and prints it to stdout:"""
+
+
+def read_file(filename=""):
+    """ Reads and prints the file content to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
