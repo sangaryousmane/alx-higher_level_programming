d9f76b722ca35f945234246d200cf024762d6445
sangaryousmane <sangaryousmane@gmail.com>
2023-09-12 19:13:45 -0700

    Dict object?

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,12 +13,4 @@ class Student:
     def def to_json(self):
         """ converts from class to json"""
         obj_dict = self.__dict__
-
-        for key, value in obj_dict.items():
-            if isinstance(value, (list, dict, str, int, bool)):
-                continue
-            elif isinstance(value, set):
-                obj_dict[key] = list(value)
-            else:
-                obj_dict[key] = str(value)
         return obj_dict
