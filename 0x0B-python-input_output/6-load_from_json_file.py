6a23aa7d75c261849453cc0a317acef7cb700ed7
sangaryousmane <sangaryousmane@gmail.com>
2023-09-12 18:49:29 -0700

    Fix load and save to file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,6 +5,6 @@ import json
 
 def load_from_json_file(filename):
     """creates an Object from a “JSON file”:"""
-    with open(filename, "w") as f:
+    with open(filename, "r") as f:
         data = json.load(f)
     return data
