7908d48d0cdcfd3ab9c03ffa26ff7895eb65a4cf
sangaryousmane <sangaryousmane@gmail.com>
2023-10-17 18:24:47 -0700

    displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+-- displays the number of records with id = 89
+-- in the table first_table of the database hbtn_0c_0 in your MySQL server.
+
+SELECT *FROM first_name WHERE id = 89;
