10c41464a5fed7870d248a447c12e478a07ae0c1
sangaryousmane <sangaryousmane@gmail.com>
2023-10-25 19:10:47 -0700

    Modify 5-unique_id.sql to work perfectly

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 -- Write a script that creates the table id_not_null on your MySQL server.
 
-CREATE TABLE IF NOT EXISTS id_not_null
+CREATE TABLE IF NOT EXISTS unique_id
 (
         id INT DEFAULT 1,
 	name VARCHAR(256)
