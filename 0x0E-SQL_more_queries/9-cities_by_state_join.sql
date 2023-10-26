42faff886f941edddf4d35500c542903fcdbc141
sangaryousmane <sangaryousmane@gmail.com>
2023-10-25 20:01:31 -0700

    update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+-- Write a script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+-- You can use only one SELECT statement
 
 SELECT c.id, c.name, s.name FROM cities AS c ON states AS s
 WHERE s.id = c.state_id ORDER BY c.id
