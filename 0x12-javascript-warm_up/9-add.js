6e802e275f19cd94291edcc23116b658c6ed85cd
sangaryousmane <sangaryousmane@gmail.com>
2023-11-13 18:49:35 -0800

    Exports add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,10 +3,4 @@ function add(a, b)
 {
 	return (a + b);
 }
-
-if (process.argv.length <= 2)
-	console.log('NaN');
-else
-{
-	console.log(add(Number(process.argv[3]), Number(process.argv[4])));
-}
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
