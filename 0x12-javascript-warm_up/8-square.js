463894fd314f9b324198c0a0f0a446a0c304fb86
sangaryousmane <sangaryousmane@gmail.com>
2023-11-13 18:27:05 -0800

    Completed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
 const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num))
-	console.log('Missing number of occurrences');
+	console.log('Missing size');
 else
 {
 	for (let i = 0; i < num; i++)
-		console.log('C is fun');
+	{
+		let sp = '';
+		for (let j = 0; j < num; j++)
+			sp += 'X';
+		console.log(sp);
+	}
 }
+
