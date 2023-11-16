b9295c8129110f138df01468e41004d9ae48c70b
sangaryousmane <sangaryousmane@gmail.com>
2023-11-15 19:08:47 -0800

    Find occurence

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-	let count = 0;
-	for (let i = 0; i < list.length; i++) {
-		if (searchElement === list[i])
-			count++;
-	}
-	return (count);
-}
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) { count++; }
+  }
+  return (count);
+};
