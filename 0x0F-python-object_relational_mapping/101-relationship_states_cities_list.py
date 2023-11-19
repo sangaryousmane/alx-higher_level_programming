836feae069841ffd2464920a8258c8072bb8f3fe
sangaryousmane <sangaryousmane@gmail.com>
2023-11-18 20:49:35 -0800

    all city and states relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     from relationship_state import Base, State
     from relationship_city import City
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session, joinedload
-
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
@@ -17,11 +17,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    states = session.query(State).options(
-        joinedload(State.cities)).order_by(State.id).all()
-    for state in states:
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
     session.close()
-
