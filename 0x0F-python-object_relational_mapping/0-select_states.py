c7b7d635f005cb8b90c4ed6d7dba953480e786b9
sangaryousmane <sangaryousmane@gmail.com>
2023-11-18 17:45:16 -0800

    Handle pep3

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,14 +5,13 @@ import MySQLdb as obj
 import sys
 
 if __name__ == "__main__":
-    
-    conn = obj.connect(user=sys.argv[1], passwd=sys.argv[2], 
-        db=sys.argv[3], host='localhost', port=3306)
 
+    conn = obj.connect(user=sys.argv[1], passwd=sys.argv[2],
+                       db=sys.argv[3], host='localhost', port=3306)
     result = conn.cursor()
     result.execute("SELECT *FROM states ORDER BY id ASC")
     query_rows = result.fetchall()
-    
+
     for row in query_rows:
         print(row)
         result.close()
