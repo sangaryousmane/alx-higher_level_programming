2c48aceafdd244e6d306c572773b7aa7d5249093
sangaryousmane <sangaryousmane@gmail.com>
2023-11-18 18:32:47 -0800

    Fix pycodestyle

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,15 +5,14 @@ import MySQLdb as obj
 import sys
 
 if __name__ == "__main__":
-    
+
     conn = obj.connect(user=sys.argv[1], passwd=sys.argv[2],
-                   db=sys.argv[3], port=3306)
+                       db=sys.argv[3], port=3306)
     result = conn.cursor()
     result.execute("""SELECT c.id, c.name, s.name FROM states s, cities c
             WHERE c.state_id = s.id ORDER BY c.id ASC""")
-
     query_rows = result.fetchall()
-    
+
     for row in query_rows:
         print(row)
     result.close()
