6aa24a7cf51d945cf3cd969803bf1d8dbb56a9b4
sangaryousmane <sangaryousmane@gmail.com>
2023-11-18 18:41:38 -0800

    Filter city given state as argument

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,16 +6,17 @@ import MySQLdb as obj
 
 
 if __name__ == "__main__":
-    conn = obj.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host='localhost', port=3306)
 
+    conn = obj.connect(user=sys.argv[1], passwd=sys.argv[2],
+                       db=sys.argv[3], port=3306)
     result = conn.cursor()
-    sql_statement = ("""
-    SELECT c.name FROM cities AS c, states s WHERE s.name %s AND c.state_id==s.id
-    ORDER BY c.id ASC""", (sys.argv[4],)
+    sql_statement = (""" SELECT c.name FROM states AS s, cities c WHERE
+            {} = %s AND c.state_id = s.id ORDER BY c.id ASC""", (sys.argv[4],))
 
-    query_rows = result.execute(sql_statement).fetchall()
+    result = result.execute(sql_statement)
+    query_rows = result.fetchall()
 
     for row in query_rows:
-        print(','.join(row[0]))
+        print(', '.join(row[0]))
     result.close()
     conn.close()
