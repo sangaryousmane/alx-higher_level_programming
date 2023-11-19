80b3bafc3ea9ce8506cb406d45f2a799fcd8237b
sangaryousmane <sangaryousmane@gmail.com>
2023-11-18 18:19:11 -0800

    Safe filter states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Yes, it’s an SQL injection to delete all records of a table…"""
 
-import sys
 import MySQLdb as obj
-
+import sys
 
 if __name__ == "__main__":
-    conn = obj.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host='localhost', port=3306)
 
+    conn = obj.connect(user=sys.argv[1], passwd=sys.argv[2],
+                       db=sys.argv[3], port=3306)
     result = conn.cursor()
-    result.execute("SELECT *FROM states WHERE name %s ORDER BY id ASC", (sys.argv[4],)
+    result.execute("SELECT *FROM states AS s WHERE s.name %s ",(sys.argv[4]),)
     query_rows = result.fetchall()
 
     for row in query_rows:
