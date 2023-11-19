d94d558aeb222fa60a37a3b6d9e8011958ad745c
sangaryousmane <sangaryousmane@gmail.com>
2023-11-18 19:21:17 -0800

    fetch all state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Write a script that prints the first State object
+from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from slqalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3], pool_pre_ping=True))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).one()
+    if len(states) == 0:
+        print("Nothing")
+        session.close()
+        break;
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
