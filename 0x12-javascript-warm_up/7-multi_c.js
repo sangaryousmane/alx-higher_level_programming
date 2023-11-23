f318a5f601c7d54553f5300d004a706cb1e4b178
sangaryousmane <sangaryousmane@gmail.com>
2023-11-22 17:44:28 -0800

    Write a script that prints x times “C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num)) { console.log('Missing number of occurrences'); } else {
-  for (let i = 0; i < num; i++) { console.log('C is fun'); }
+  for (let i = 1; i <= num; i++) { console.log('C is fun'); }
 }
