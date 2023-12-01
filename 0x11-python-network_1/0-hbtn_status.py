6339bfee315eee2044fa2c5dfe798b140bdeb3c0
sangaryousmane <sangaryousmane@gmail.com>
2023-11-30 20:43:53 -0800

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" What's my status? #0"""
+from urllib.request import urlopen
+
+
+if __name__ == '__main__':
+    with urlopen("https://alx-intranet.hbtn.io/status") as res:
+        page = res.read()
+        print("Body Response: ")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode("utf-8")))
