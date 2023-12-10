33a93630d12b570b5d74211b5950de29afdca869
sangaryousmane <sangaryousmane@gmail.com>
2023-12-09 18:46:47 -0800

    Find the second biggest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) { console.log(0); } else if (args.length === 1) { console.log(1); } else {
+  let max = parseInt(args[0]);
+  let secondMax = parseInt(args[1]);
+
+  if (secondMax > max) { [max, secondMax] = [secondMax, max]; }
+
+  for (let i = 2; i < args.length; i++) {
+    const num = parseInt(args[i]);
+
+    if (num > max) {
+      secondMax = max;
+      max = num;
+    } else if (num > secondMax) { secondMax = num; }
+  }
+  console.log(secondMax);
+}
