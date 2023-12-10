2065e49b12c9d18d23740d4283201275e187c8e5
sangaryousmane <sangaryousmane@gmail.com>
2023-12-09 18:49:28 -0800

    Find the second biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 const args = process.argv.slice(2);
 
-if (args.length === 0) { console.log(0); } else if (args.length === 1) { console.log(1); } else {
+if (args.length === 0) { console.log(0); } else if (args.length === 1) { console.log(0); } else {
   let max = parseInt(args[0]);
   let secondMax = parseInt(args[1]);
 
