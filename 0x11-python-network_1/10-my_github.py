8d332c2e3c62fd76acf96b2399750ea353538749
sangaryousmane <sangaryousmane@gmail.com>
2023-12-30 18:46:01 -0800

    Save changes

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,6 +10,11 @@ from requests.auth import HTTPBasicAuth
 if __name__ == '__main__':
     args = sys.argv[1:]
     url = 'https://api.github.com/users/{}'.format(args[0])
-
     response = requests.get(url, auth=HTTPBasicAuth(args[0], args[1]))
-    print(response.json().get('id'))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data.get('id')
+        print(user_id)
+    else:
+        print(f"Failed to fetch user ID. Status code: {response.status_code}")
