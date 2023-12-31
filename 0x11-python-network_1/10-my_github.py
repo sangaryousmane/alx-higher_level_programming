0f582b2e05bb58d62b49bc64fbd67cb386a2616e
sangaryousmane <sangaryousmane@gmail.com>
2023-12-30 18:43:43 -0800

    Little modification

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,22 +7,9 @@ import sys
 from requests.auth import HTTPBasicAuth
 
 
-def get_user_id(username, password):
-    url = 'https://api.github.com/users/{}'.format(username)
-    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    url = 'https://api.github.com/users/{}'.format(args[0])
 
-    if response.status_code == 200:
-        user_data = response.json()
-        user_id = user_data.get('id')
-        print(user_id)
-    else:
-        print(f"Failed to fetch user ID. Status code: {response.status_code}")
-
-
-if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        get_user_id(username, password)
-    else:
-        print("Please provide both username and password")
+    response = requests.get(url, auth=HTTPBasicAuth(args[0], args[1]))
+    print(response.json().get('id'))
