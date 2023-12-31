cab9bcbdab0c045bdf501d9d1f255a37779bc2ad
sangaryousmane <sangaryousmane@gmail.com>
2023-12-30 17:45:05 -0800

    Modify formatting

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
 
     res = requests.get("https://alx-intranet.hbtn.io/status")
     for line in res.text.split("\n"):
-        print('Body response: ')
-        print('\t- type :{}'.format(type(line)))
-        print('\t- content :{}'.format(line))
+        print('Body response:')
+        print('\t- type: {}'.format(type(line)))
+        print('\t- content: {}'.format(line))
