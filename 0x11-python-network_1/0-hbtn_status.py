62bdf4510d5dc2ecba6a784bef9873692b9b2456
sangaryousmane <sangaryousmane@gmail.com>
2023-12-30 17:35:04 -0800

    Made some modifications to the status response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ from urllib.request import urlopen
 if __name__ == '__main__':
     with urlopen("https://alx-intranet.hbtn.io/status") as res:
         page = res.read()
-        print("Body Response: ")
+        print("Body Response:")
         print("\t- type: {}".format(type(page)))
         print("\t- content: {}".format(page))
         print("\t- utf8 content: {}".format(page.decode("utf-8")))
