58e86d62798509b25281376ae80cb91ddea1d2af
sangaryousmane <sangaryousmane@gmail.com>
2023-12-30 18:09:55 -0800

    Add a json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This script takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+def search_user(letter):
+    if letter:
+        payload = {'q': letter}
+    else:
+        payload = {'q': ''}
+
+    response = requests.post('http://0.0.0.0:5000/search_user', params=payload)
+
+    try:
+        json_data = response.json()
+        if json_data:
+            print(f"[{json_data['id']}] {json_data['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        query_letter = sys.argv[1]
+    else:
+        query_letter = ""
+
+    search_user(query_letter)
+
