feeec8d356f69ed7eaceb3510eb8fde457da38d9
sangaryousmane <sangaryousmane@gmail.com>
2023-12-30 18:28:20 -0800

    Github search

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id"""
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+
+def get_user_id(username, password):
+    url = 'https://api.github.com/user/{}'.format(username)
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data.get('id')
+        print(user_id)
+    else:
+        print(f"Failed to fetch user ID. Status code: {response.status_code}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        get_user_id(username, password)
+    else:
+        print("Please provide both username and password (personal access token) as arguments.")
+
