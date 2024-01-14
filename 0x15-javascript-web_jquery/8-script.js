dd10eb7a93f851380627d93e112c0df6df62205c
sangaryousmane <sangaryousmane@gmail.com>
2024-01-13 21:15:52 -0800

    Translate french to english

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,17 +1,16 @@
-// This script fetches and lists the title for all movies 
+// This script fetches and lists the title for all movies
 
 $.ajax({
-	url: "https://swapi-api.alx-tools.com/api/films/?format=json",
-	method: "GET"
-	dataType: "JSON",
-	success: function(res){
-		const movies = res.results;
-		movies.forEach(movie => {
-		$('UL#list_movies').append('<li>' + movie.title + '</li>');
-		)};
-	},
-	error: function(err){
-		console.error(err);
-	}
-})
-
+  url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+  method: 'GET',
+  dataType: 'JSON',
+  success: function (res) {
+    const movies = res.results;
+    movies.forEach(movie => {
+      $('ul#list_movies').append('<li>' + movie.title + '</li>');
+    });
+  },
+  error: function (err) {
+    console.error(err);
+  }
+});
