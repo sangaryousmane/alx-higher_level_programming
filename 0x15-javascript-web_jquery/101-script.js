8e67780191eb710b6328bd4d151c48fdf4d761ee
sangaryousmane <sangaryousmane@gmail.com>
2024-01-13 21:39:36 -0800

    Do when the script is ready

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+// This script that adds, removes and clears - 
+// li elements from a list when the user clicks
+
+$(document).ready(function(){
+	const myList = $("ul.my_list");
+
+	$("#add_item").on("click", () => myList.append('<li>Item</li>'));
+
+	$("#clear_list").on("click", () => myList.empty());
+
+	$("#remove_item").on("click", () => $("ul.my_list li:last-child").remove());
+});
